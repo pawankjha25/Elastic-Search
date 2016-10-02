3fb4f7794ff40300cf9739d93b4e79035b1373ce
girish-kumar-710 <girish.kumar@razorthink.net>
2016-10-02 18:06:20 +0530

    Import data from new csv file

--- src/main/java/com/searchApplication/schedulers/ElasticSearchIndexComponent.java
+++ src/main/java/com/searchApplication/schedulers/ElasticSearchIndexComponent.java
@@ -33,7 +33,9 @@ public class ElasticSearchIndexComponent {
 	{
 		try
 		{
-			indexCSVFileData();
+			//indexCSVFileData();
+			System.out.println("Adding old data");
+			//indexOldCSVFileData();
 		}
 		catch( Exception e )
 		{
@@ -42,9 +44,9 @@ public class ElasticSearchIndexComponent {
 	}
 
 	@SuppressWarnings( "resource" )
-	public void indexCSVFileData() throws Exception
+	public void indexOldCSVFileData() throws Exception
 	{
-		String csvFile = "/home/girish/zdalydata_sample.csv ";
+		String csvFile = "/home/girish/zdalydata_sample.csv";
 		BufferedReader br = null;
 		String line = "";
 		String cvsSplitBy = ">>>";
@@ -64,7 +66,7 @@ public class ElasticSearchIndexComponent {
 					String[] description = line.split(cvsSplitBy);
 					if( description.length == 5 )
 					{
-						TimeSeriesData data = convertData(description[0].trim(), description[1].trim(),
+						TimeSeriesData data = convertOldData(description[0].trim(), description[1].trim(),
 								description[2].trim(), description[3].trim(), description[4].trim());
 						list.add(data);
 					}
@@ -72,7 +74,6 @@ public class ElasticSearchIndexComponent {
 					{
 						elasticSearchUtility.addDocsInBulk(elasticSearchUtility.getESClient(), "zdaly", "time_series",
 								list);
-						System.out.println(i);
 						list.removeAll(list);
 					}
 				}
@@ -85,7 +86,7 @@ public class ElasticSearchIndexComponent {
 		}
 	}
 
-	public static TimeSeriesData convertData( String propertyidDB, String attributes, String attributesMetaData,
+	public static TimeSeriesData convertOldData( String propertyidDB, String attributes, String attributesMetaData,
 			String locations, String locationsMetaData ) throws Exception
 	{
 		TimeSeriesData data = new TimeSeriesData();
@@ -204,6 +205,175 @@ public class ElasticSearchIndexComponent {
 		catch( Exception e )
 		{
 			throw e;
+		}
+		return data;
+	}
+
+	@SuppressWarnings( "resource" )
+	public void indexCSVFileData() throws Exception
+	{
+		String csvFile = "/ebs/apps/QuickStats_Attributes_Data.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ">>>";
+		try
+		{
+
+			br = new BufferedReader(new FileReader(csvFile));
+			int i = 0;
+			List<TimeSeriesData> list = new ArrayList<TimeSeriesData>();
+			while( (line = br.readLine()) != null )
+			{
+				if( i > 83 )
+				{
+					line = line.replace("\",\"", ">>>");
+					line = line.replace("\"", "");
+					line = line.replace("]", "").replace("[", "");
+
+					String[] description = line.split(cvsSplitBy);
+					if( description.length == 8 )
+					{
+						TimeSeriesData data = convertData(description[0].trim(), description[1].trim(),
+								description[2].trim(), description[3].trim(), description[4].trim(),
+								description[7].trim());
+						list.add(data);
+					}
+					if( i % 100 == 0 )
+					{
+						elasticSearchUtility.addDocsInBulk(elasticSearchUtility.getESClient(), "zdaly", "time_series",
+								list);
+						list.removeAll(list);
+					}
+				}
+				i++;
+			}
+		}
+		catch( Exception e )
+		{
+			throw e;
+		}
+	}
+
+	public static TimeSeriesData convertData( String propertyidDB, String attributes, String attributesMetaData,
+			String locations, String locationsMetaData, String propertyId ) throws Exception
+	{
+		System.out.println("------------------");
+		TimeSeriesData data = new TimeSeriesData();
+		try
+		{
+			List<Attributes> attributeList = new ArrayList<Attributes>();
+			DatabaseInfo db = new DatabaseInfo();
+			Set<Locations> locationList = new TreeSet<Locations>();
+			List<String> description = new ArrayList<String>();
+
+			// setting up the db information
+			db.setDb_name(propertyidDB);
+			db.setProperties(new Long(propertyId));
+			data.setDb(db);
+
+			// setting up the attributes information
+			String[] attributesValuesList = attributes.replace("|", ">>>").split(">>>");
+			int attLength = attributesValuesList.length;
+			String[] attributesMetadataList = attributesMetaData.trim().replace("|", ">>>").split(">>>");
+			String[] attributesNameList = new String[attributesMetadataList.length];
+			String[] attributesTypeList = new String[attributesMetadataList.length];
+
+			for( int i = 0; i < attLength; i++ )
+			{
+				attributesNameList[i] = attributesMetadataList[i].substring(attributesMetadataList[i].indexOf("(") + 1,
+						attributesMetadataList[i].indexOf(")"));
+
+				attributesTypeList[i] = attributesMetadataList[i].substring(0, attributesMetadataList[i].indexOf("("));
+			}
+
+			for( int i = 0; i < attLength; i++ )
+			{
+				Attributes att = new Attributes();
+				att.setAttribute_name(attributesTypeList[i].trim());
+				att.setAttribute_value(attributesValuesList[i].trim());
+				if( attributesNameList[i].split(",")[1] != null
+						&& !attributesNameList[i].split(",")[1].equalsIgnoreCase("NULL") )
+				{
+					att.setAttribute_level(new Long(attributesNameList[i].split(",")[1]));
+				}
+				else
+				{
+					att.setAttribute_level((long) 0);
+				}
+				att.setAttribute_parent(attributesNameList[i].split(",")[0].trim());
+
+				attributeList.add(att);
+
+				// add sector subsector and super region
+				if( att.getAttribute_name().equalsIgnoreCase("sector") )
+				{
+					data.setSector(att.getAttribute_value().trim());
+				}
+				else if( att.getAttribute_name().equalsIgnoreCase("SubSector") )
+				{
+					data.setSub_sector(att.getAttribute_value().trim());
+				}
+				// add description
+				description.add(att.getAttribute_value().trim());
+			}
+			data.setAttributes(attributeList);
+
+			// setting up the locations information
+			String[] locationValuesList = locations.replace("|", ">>>").split(">>>");
+			String[] locationMetaValuesList = locationsMetaData.replace("|", ">>>").split(">>>");
+			System.out.println(locationValuesList.length);
+
+			// iterate through all the location details
+			for( int i = 0; i < locationValuesList.length; i++ )
+			{
+				String[] loc = locationValuesList[i].split(",");
+				String[] locMeta = locationMetaValuesList[i].split(",");
+
+				Locations location = new Locations();
+				if( loc.length == locMeta.length )
+				{
+					for( int j = 0; j < loc.length; j++ )
+					{
+						if( locMeta[j].trim().equalsIgnoreCase("Super Region") )
+						{
+							data.setSuper_region(loc[j].trim());
+						}
+						else
+						{
+							location.setLocation_meta("");
+							location.setLocation_parent(loc[j - 1].trim());
+							if( loc[j].contains("(") )
+							{
+								location.setLocation_name(loc[j].substring(0, loc[j].indexOf("(")).trim());
+							}
+							else
+							{
+								location.setLocation_name(loc[j].trim());
+							}
+							location.setLocation_type(locMeta[j].trim());
+							if( loc[loc.length - 1].contains("(") )
+							{
+								String lastLoc = loc[loc.length - 1].substring(0, loc[loc.length - 1].indexOf("("))
+										.trim();
+								String id = loc[loc.length - 1].replaceFirst(lastLoc, "").replace("(", "")
+										.replace(")", "").trim();
+								if( !id.trim().isEmpty() )
+								{
+									location.setSeries_id(new Long(id));
+									locationList.add(location);
+								}
+							}
+						}
+
+					}
+				}
+			}
+			data.setLocations(locationList);
+			data.setDescription(description);
+		}
+		catch( Exception e )
+		{
+
 		}
 		return data;
 	}
