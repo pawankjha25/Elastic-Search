f86b7993079ab2d316913c8534b3190761166ab9
ja-ilija <ilija@nomnom.it>
2016-10-05 06:40:36 -0300

    some changes

--- src/main/java/com/searchApplication/es/indexing/CSVParser.java
+++ src/main/java/com/searchApplication/es/indexing/CSVParser.java
@@ -1,8 +1,140 @@
 package com.searchApplication.es.indexing;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.searchApplication.es.entities.Attributes;
+import com.searchApplication.es.entities.DatabaseInfo;
+import com.searchApplication.es.entities.Locations;
+import com.searchApplication.es.entities.TimeSeriesData;
+
 public class CSVParser {
 
-	private static TimeSeriesData parseLine(String line) {
+
+	private static final String SPLITER = ">>>";
+
+	private static TimeSeriesData parseLine(String line) throws Exception {
+
+		line = line.replaceAll("\",\"", SPLITER);
+		line = line.replaceAll("\"", "");
+		line = line.replaceAll("]", "").replaceAll("[", "");
+
+		String[] description = line.split(SPLITER);
+		if (description.length == 8) {
+			TimeSeriesData data = convertData(description[0].trim(), description[1].trim(), description[2].trim(),
+					description[3].trim(), description[4].trim(), description[7].trim());
+			return data;
+		} else {
+			return null;
+		}
+	}
+	
+	public static List<Attributes> getAttriuteData(String attributes, String attributesMetaData) {
+		List<Attributes> attributeList = new ArrayList<Attributes>();
+
+		String[] attributesValuesList = attributes.replace("|", SPLITER).split(SPLITER);
+		int attLength = attributesValuesList.length;
+		String[] attributesMetadataList = attributesMetaData.trim().replace("|", SPLITER).split(SPLITER);
+		String[] attributesNameList = new String[attributesMetadataList.length];
+		String[] attributesTypeList = new String[attributesMetadataList.length];
+
+		for (int i = 0; i < attLength; i++) {
+			attributesNameList[i] = attributesMetadataList[i].substring(attributesMetadataList[i].indexOf("(") + 1,
+					attributesMetadataList[i].indexOf(")"));
+
+			attributesTypeList[i] = attributesMetadataList[i].substring(0, attributesMetadataList[i].indexOf("("));
+		}
+
+		for (int i = 0; i < attLength; i++) {
+			Attributes att = new Attributes();
+			att.setAttribute_name(attributesTypeList[i].trim());
+			att.setAttribute_value(attributesValuesList[i].trim());
+			if (attributesNameList[i].split(",")[1] != null
+					&& !attributesNameList[i].split(",")[1].equalsIgnoreCase("NULL")) {
+				att.setAttribute_level(new Long(attributesNameList[i].split(",")[1]));
+			} else {
+				att.setAttribute_level((long) 0);
+			}
+			att.setAttribute_parent(attributesNameList[i].split(",")[0].trim());
+
+			attributeList.add(att);
+
+			// add sector subsector and super region
+			if (att.getAttribute_name().equalsIgnoreCase("sector")) {
+				data.setSector(att.getAttribute_value().trim());
+			} else if (att.getAttribute_name().equalsIgnoreCase("SubSector")) {
+				data.setSub_sector(att.getAttribute_value().trim());
+			}
+			// add description
+			description.add(att.getAttribute_value().trim());
+		}
+	}
+
+	public static TimeSeriesData convertData(String propertyidDB, String attributes, String attributesMetaData,
+			String locations, String locationsMetaData, String propertyId) throws Exception {
+		System.out.println("------------------");
+		TimeSeriesData data = new TimeSeriesData();
+		try {
+			List<Attributes> attributeList = new ArrayList<Attributes>();
+			DatabaseInfo db = new DatabaseInfo();
+			Set<Locations> locationList = new TreeSet<Locations>();
+			List<String> description = new ArrayList<String>();
+
+			// setting up the db information
+			db.setDb_name(propertyidDB);
+			db.setProperties(new Long(propertyId));
+			data.setDb(db);
+
+			// setting up the attributes information
 		
+			data.setAttributes(attributeList);
+
+			// setting up the locations information
+			String[] locationValuesList = locations.replace("|", SPLITER).split(SPLITER);
+			String[] locationMetaValuesList = locationsMetaData.replace("|", SPLITER).split(SPLITER);
+			System.out.println(locationValuesList.length);
+
+			// iterate through all the location details
+			for (int i = 0; i < locationValuesList.length; i++) {
+				String[] loc = locationValuesList[i].split(",");
+				String[] locMeta = locationMetaValuesList[i].split(",");
+
+				Locations location = new Locations();
+				if (loc.length == locMeta.length) {
+					for (int j = 0; j < loc.length; j++) {
+						if (locMeta[j].trim().equalsIgnoreCase("Super Region")) {
+							data.setSuper_region(loc[j].trim());
+						} else {
+							location.setLocation_meta("");
+							location.setLocation_parent(loc[j - 1].trim());
+							if (loc[j].contains("(")) {
+								location.setLocation_name(loc[j].substring(0, loc[j].indexOf("(")).trim());
+							} else {
+								location.setLocation_name(loc[j].trim());
+							}
+							location.setLocation_type(locMeta[j].trim());
+							if (loc[loc.length - 1].contains("(")) {
+								String lastLoc = loc[loc.length - 1].substring(0, loc[loc.length - 1].indexOf("("))
+										.trim();
+								String id = loc[loc.length - 1].replaceFirst(lastLoc, "").replace("(", "")
+										.replace(")", "").trim();
+								if (!id.trim().isEmpty()) {
+									location.setSeries_id(new Long(id));
+									locationList.add(location);
+								}
+							}
+						}
+
+					}
+				}
+			}
+			data.setLocations(locationList);
+			data.setDescription(description);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return data;
 	}
 }
