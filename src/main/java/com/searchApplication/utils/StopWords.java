234d25d7fc531dff08442e5a356b49c4ad2d13e8
ja-ilija <ilija@nomnom.it>
2016-10-17 11:48:59 -0300

    add stopwords

---
+++ src/main/java/com/searchApplication/utils/StopWords.java
@@ -0,0 +1,12 @@
+package com.searchApplication.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StopWords {
+
+	public static List<String> STOP_LIST = Arrays.asList("a", "an", "and", "are", "as", "at", "be", "but", "by", "for",
+			"if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then",
+			"there", "these", "they", "this", "to", "was", "will", "with", "more");
+
+}
