f8bc9f5a6c6cc932207fd84e24dc492119e8cb39
ja-ilija <ilija@nomnom.it>
2016-11-20 23:13:47 -0800

    add aggregaition files

---
+++ src/main/java/com/searchApplication/es/search/bucketing/Aggregator.java
@@ -0,0 +1,104 @@
+package com.searchApplication.es.search.bucketing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Aggregator {
+
+	public static List<Bucket> generateAggregated(List<Bucket> buckets) {
+		Bucket[] aggregatedBuckets = new Bucket[buckets.size()];
+		List<Bucket> resultBuckets = new ArrayList<Bucket>();
+		HashMap<String, AggregatedBucket> bucketCombination = new HashMap<String, AggregatedBucket>();
+		for (int i=0; i< buckets.size(); i++) {
+			List<BucketTerms> l = generateCandidate(buckets.get(i));
+			if (l != null) {
+				checkAggregatedBucket(bucketCombination, l, i, buckets.get(i).getTotalRows());
+			}
+
+		}
+		List<Integer> aggregatedBucketsList = new ArrayList<Integer>();
+		for (Map.Entry<String, AggregatedBucket> entry : bucketCombination.entrySet()) {
+			if (entry.getValue().getBuckets().size() > 1) {
+				System.out.println("Comb " + entry.getKey() + "  " + entry.getValue().getBuckets());
+				Bucket c = new Bucket(entry.getValue().getBucketTerms(),
+						buckets.get(entry.getValue().getFirstAppearance()).getTotalPerfectMatches(), 0, 0);
+				c.setTotalRows(entry.getValue().getCount());
+				c.setBucketMetaData(buckets.get(entry.getValue().getFirstAppearance()).getBucketMetaData());
+				aggregatedBuckets[entry.getValue().getFirstAppearance()] = c;
+				aggregatedBucketsList.addAll(entry.getValue().getBuckets());
+
+			}
+		}
+		for (int i = 0; i < buckets.size(); i++) {
+
+			if (aggregatedBuckets[i] != null) {
+				resultBuckets.add(aggregatedBuckets[i]);
+			} else if (!aggregatedBucketsList.contains(i)) {
+				resultBuckets.add(buckets.get(i));
+			} 
+		}
+		return resultBuckets;
+	}
+
+	public static List<BucketTerms> generateCandidate(Bucket b) {
+		int totalMatch = 0;
+		Set<String> matched = new HashSet<String>();
+		List<BucketTerms> aggTerms = new ArrayList<BucketTerms>();
+		for (BucketTerms bt : b.getBucketTerms()) {
+			boolean forAdd = false;
+			// TODO this orders differently if there is a multi match bucket
+			// after a single match attribute
+			if (bt.getMatchedQueries().size() == b.getTotalPerfectMatches()) {
+				return Arrays.asList(bt);
+			} else {
+				for (String m : bt.getMatchedQueries()) {
+					if (!matched.contains(m)) {
+						totalMatch++;
+						matched.add(m);
+						forAdd = true;
+					}
+				}
+
+				if (forAdd) {
+					aggTerms.add(bt);
+				}
+				if (totalMatch == b.getTotalPerfectMatches()) {
+					break;
+				}
+			}
+		}
+		if (totalMatch >= b.getTotalPerfectMatches()) {
+			return aggTerms;
+		} else {
+			return null;
+		}
+
+	}
+
+	private static void checkAggregatedBucket(HashMap<String, AggregatedBucket> bucketCombination,
+			List<BucketTerms> combs, int index, long totalRows) {
+
+		String comb = "";
+		for (BucketTerms b : combs) {
+			comb += b.getAttributeName() + " | ";
+		}
+		if (bucketCombination.containsKey(comb)) {
+			bucketCombination.get(comb).incrementCounts(totalRows);
+			bucketCombination.get(comb).addIndex(index);
+
+		} else {
+			AggregatedBucket b = new AggregatedBucket();
+			b.setBucketTerms(new HashSet<BucketTerms>(combs));
+			b.setCount(1);
+			b.setFirstAppearance(index);
+			b.addIndex(index);
+			bucketCombination.put(comb, b);
+		}
+	}
+
+}
---
+++ src/test/java/com/searchApplication/es/search/bucketing/AggregatorTest.java
@@ -0,0 +1,68 @@
+package com.searchApplication.es.search.bucketing;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.fest.assertions.api.Assertions;
+import org.junit.Test;
+
+public class AggregatorTest {
+
+	@Test
+	public void test() {
+		Set<String> queryMatchA = new HashSet<String>(Arrays.asList("a"));
+		Set<String> queryMatchB = new HashSet<String>(Arrays.asList("b"));
+		Set<String> queryMatchAB = new HashSet<String>(Arrays.asList("a", "b"));
+
+		BucketTerms bta = new BucketTerms("a", 1, true, 1);
+		bta.setMatchedQueries(queryMatchA);
+		bta.setQueryWordMatch(1);
+
+		BucketTerms bta1 = new BucketTerms("b", 1, true, 1);
+		bta1.setMatchedQueries(queryMatchB);
+		bta.setQueryWordMatch(1);
+
+		BucketTerms bta2 = new BucketTerms("a b", 1, true, 1);
+		bta2.setMatchedQueries(queryMatchAB);
+		bta2.setQueryWordMatch(2);
+		BucketTerms bta3 = new BucketTerms("a3", 1, true, 1);
+		bta3.setMatchedQueries(queryMatchA);
+
+		BucketTerms bta4 = new BucketTerms("a4", 1, true, 1);
+		bta4.setMatchedQueries(queryMatchA);
+
+		BucketTerms bta5 = new BucketTerms("a5", 1, true, 1);
+		bta5.setMatchedQueries(queryMatchA);
+
+		Bucket b = new Bucket(new HashSet<>(Arrays.asList(bta, bta1)), 2, 0, 10);
+		Bucket b1 = new Bucket(new HashSet<>(Arrays.asList(bta1, bta, bta2)), 2, 0, 10);
+		Bucket b2 = new Bucket(new HashSet<>(Arrays.asList(bta2, bta1)), 2, 0, 10);
+		Bucket b3 = new Bucket(new HashSet<>(Arrays.asList(bta2, bta3)), 2, 0, 1000);
+
+		Bucket b4 = new Bucket(new HashSet<>(Arrays.asList(bta4)), 1, 0, 0);
+		Bucket b5 = new Bucket(new HashSet<>(Arrays.asList(bta5)), 1, 0, 1);
+
+		List<Bucket> list = Arrays.asList(b, b1, b2, b3, b4, b5);
+		List<Bucket> aggegatedList = Aggregator.generateAggregated(list);
+		Assertions.assertThat(aggegatedList).hasSize(4);
+		String[] bucketsNames = new String[aggegatedList.size()];
+		for (int i = 0; i < aggegatedList.size(); i++) {
+			String x = "";
+			for (BucketTerms bt : aggegatedList.get(i).getBucketTerms()) {
+				x += bt.getAttributeName() + "|";
+			}
+			bucketsNames[i] = x;
+		}
+		Assertions.assertThat(bucketsNames[0]).isEqualTo("a|b|");
+		Assertions.assertThat(bucketsNames[1]).isEqualTo("a b|");
+		Assertions.assertThat(bucketsNames[2]).isEqualTo("a4|");
+		Assertions.assertThat(bucketsNames[3]).isEqualTo("a5|");
+
+
+	}
+
+}
