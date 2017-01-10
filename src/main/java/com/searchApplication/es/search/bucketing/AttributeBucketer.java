51d061ac21257d91df7a47cc33e2ee539c185bf4
ja-ilija <ilija@nomnom.it>
2017-01-10 13:04:59 -0800

    change ordr

--- src/main/java/com/searchApplication/es/search/bucketing/AttributeBucketer.java
+++ src/main/java/com/searchApplication/es/search/bucketing/AttributeBucketer.java
@@ -1,5 +1,6 @@
 package com.searchApplication.es.search.bucketing;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,23 +27,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.searchApplication.es.entities.BucketResponseList;
+import com.searchApplication.es.search.bucketing.bucketeers.ESAggregationBucketeer;
+import com.searchApplication.es.search.bucketing.bucketeers.ESHitBucketeer;
+import com.searchApplication.es.search.bucketing.wordnet.WordNetSynonims;
+
+import edu.mit.jwi.morph.WordnetStemmer;
 
 public class AttributeBucketer {
 
 	private static final String LOCATION_CONTEXT = "_LOC";
-
-	private static final String SHINGLE_ANALYZER = "shingle_analyzer";
-
 	private static final Logger LOGGER = LoggerFactory.getLogger(AttributeBucketer.class);
-
-	private static final String LOCATION_NAME = "location_name";
 	private static final String LOCATIONS = "locations";
-	private static final int HITS_IN_SCROLL = 500;
 	private static final String SEARCH_FIELD = "description.shingled";
-	private static final String N_GRAM_ANALYZER = "n_gram_analyzer";
 
 	public static BucketResponseList generateBuckets(Client client, String index, String type, String query, int loops,
-			int hitsInScroll, Set<String> locations) {
+			int hitsInScroll, Set<String> locations) throws IOException {
 		List<Bucket> buckets = aggregateBuckets(
 				createBucketList(client, index, type, query, loops, hitsInScroll, locations));
 		return BucketResponseList.buildFromBucketList(buckets, query);
@@ -69,7 +68,7 @@ public class AttributeBucketer {
 	}
 
 	public static List<Bucket> createBucketList(Client client, String index, String type, String query, int loops,
-			int hitsInScroll, Set<String> locations) {
+			int hitsInScroll, Set<String> locations) throws IOException {
 
 		LOGGER.debug("Start query ");
 		String[] querySplit = generateAttAndLocQueries(cleanQuery(query), locations, 1);
@@ -87,120 +86,54 @@ public class AttributeBucketer {
 	}
 
 	private static List<Bucket> getBucketsFromSearch(String[] querySplit, int hitsInScroll, int loops, Client client,
-			String index, String type) {
-
+			String index, String type) throws IOException {
+		List<Bucket> bucketList;
 		if (querySplit[0].split(" ").length == 1 && querySplit[1].split(" ").length <= 1) {
 			SearchResponse sr = hitEsSingle(client, index, type, hitsInScroll, querySplit);
-			List<Bucket> bucketList = getBucketsFromSearchResponseWithAgg(sr, querySplit, hitsInScroll, loops, client);
-			return bucketList;
+			bucketList = ESAggregationBucketeer.getBucketsFromSearchResponseWithAgg(sr, querySplit, hitsInScroll, loops,
+					client);
 		} else {
 			SearchResponse sr = hitEsMulti(client, index, type, hitsInScroll, querySplit);
-			List<Bucket> bucketList = getBucketsFromSearchResponse(sr, querySplit, hitsInScroll, loops, client);
+			bucketList = ESHitBucketeer.getBucketsFromSearchResponse(sr, querySplit, hitsInScroll, loops, client);
+
+		}
+		if (bucketList.size() > 0) {
 			return bucketList;
+		} else {
+			return doSynonimSearch(querySplit, hitsInScroll, loops, client, index, type);
 		}
 	}
 
-	private static List<Bucket> getBucketsFromSearchResponseWithAgg(SearchResponse sr, String[] querySplit,
-			int hitsInScroll, int loops, Client client) {
-		int hitCounter = 0;
+	private static List<Bucket> doSynonimSearch(String[] querySplit, int hitsInScroll, int loops, Client client,
+			String index, String type) throws IOException {
 		List<Bucket> bucketList = new ArrayList<Bucket>();
-		Set<String> hits = new HashSet<String>();
-		LOGGER.debug(" query {}", sr.toString());
 
-		LOGGER.debug(" response {} {} {}", hitCounter, sr.getHits().getHits().length, sr.getTookInMillis());
-
-		Iterator<org.elasticsearch.search.aggregations.bucket.terms.Terms.Bucket> buckets = ((StringTerms) sr
-				.getAggregations().asMap().get("values")).getBuckets().iterator();
-
-		while (buckets.hasNext()) {
-			Terms.Bucket b = buckets.next();
-			Bucket result = BucketBuilders.createFromQueryString(querySplit[0], Arrays.asList(b.getKeyAsString()),
-					hits);
-			if (result == null && querySplit[1].length() == 0) {
-				continue;
-			} else if (result == null && querySplit[1].length() > 0) {
-				BucketTerms bts = new BucketTerms(querySplit[1].toUpperCase() + LOCATION_CONTEXT, 100, true, 0);
-				result = new Bucket(new HashSet(Arrays.asList(bts)), 1, 1, 0);
-			} else if (result != null && querySplit.length > 1 && querySplit[1].length() > 1) {
-				BucketTerms bts = new BucketTerms(querySplit[1].toUpperCase() + LOCATION_CONTEXT, 100, true, 0);
-				result.getBucketTerms().add(bts);
-
+		List<List<String>> queries = new ArrayList<List<String>>();
+		for (String q : querySplit[0].split(" ")) {
+			if (q.length() > 2) {
+				List<String> qs = WordNetSynonims.getSingleton().testDictionary(q);
+				queries.add(qs);
 			}
-			Iterator<org.elasticsearch.search.aggregations.bucket.terms.Terms.Bucket> sectorIt = ((StringTerms) b
-					.getAggregations().asList().get(0)).getBuckets().iterator();
-			List<BucketMetaData> metaDataList = new ArrayList<BucketMetaData>();
-			while (sectorIt.hasNext()) {
-				Terms.Bucket sectorBucket = sectorIt.next();
-				Iterator<org.elasticsearch.search.aggregations.bucket.terms.Terms.Bucket> subSectorIT = ((StringTerms) sectorBucket
-						.getAggregations().asList().get(0)).getBuckets().iterator();
-				while (subSectorIT.hasNext()) {
-					Terms.Bucket subSectorBucket = subSectorIT.next();
-					Iterator<org.elasticsearch.search.aggregations.bucket.terms.Terms.Bucket> superRegionIT = ((StringTerms) subSectorBucket
-							.getAggregations().asList().get(0)).getBuckets().iterator();
-
-					while (superRegionIT.hasNext()) {
-						Terms.Bucket regionBucket = superRegionIT.next();
-						BucketMetaData metaData = new BucketMetaData(regionBucket.getKeyAsString(),
-								sectorBucket.getKeyAsString(), subSectorBucket.getKeyAsString());
-						metaData.setTotal(regionBucket.getDocCount());
-						LOGGER.debug("adding metadata {}", metaData);
-						metaDataList.add(metaData);
-					}
-				}
-			}
-			LOGGER.debug("bucket {}", result);
-			result.setBucketMetaData(metaDataList);
-			result.setTotalRows(b.getDocCount());
-			bucketList.add(result);
-
 		}
-
-		Collections.sort(bucketList);
-		LOGGER.debug(" list {}", bucketList);
-
+		List<String> synonimQueries = new ArrayList<String>();
+		Permuattions.generatePermutations(queries, synonimQueries, 0, "");
+		int synCounter = 0;
+		while (synCounter < synonimQueries.size()) {
+			synCounter++;
+			String[] synQueries = new String[2];
+			synQueries[1] = querySplit[1];
+			synQueries[0] = synonimQueries.get(synCounter).replaceAll("_", " ");
+			bucketList = getBucketsFromSearch(synQueries, hitsInScroll, loops, client, index, type);
+			if (bucketList.isEmpty()) {
+				break;
+			}
+			
+		}
 		return bucketList;
 	}
 
 	private static String cleanQuery(String query) {
 		return query.toLowerCase().trim().replaceAll("\\p{P}", "").trim();
-	}
-
-	private static Bucket processHitsToBuckets(SearchHit hit, String query, Set<String> checked, Set<String> misses) {
-		List<String> bucketTerms = new ArrayList<String>();
-		BucketMetaData metaData = new BucketMetaData((String) hit.getSource().get("super_region"),
-				(String) hit.getSource().get("sector"), (String) hit.getSource().get("sub_sector"));
-		if (hit.getSource() != null && hit.getSource().containsKey("attributes")) {
-			try {
-				for (Map<String, String> attributeData : (List<Map<String, String>>) hit.getSource()
-						.get("attributes")) {
-					if (!misses.contains(attributeData.get("attribute_value"))) {
-						if (attributeData.get("attribute_value") != null) {
-							bucketTerms.add(attributeData.get("attribute_value"));
-						} else {
-							LOGGER.debug("Attribute value is NULL");
-						}
-					}
-				}
-			} catch (Exception e) {
-				LOGGER.debug("skipped attribute");
-			}
-		}
-		Bucket b = null;
-		if (!query.equals("")) {
-			b = BucketBuilders.createFromQueryString(query, bucketTerms, checked);
-
-		} else {
-			b = new Bucket(new HashSet<BucketTerms>(), 0, 0, 0);
-		}
-
-		if (b != null) {
-			
-			List<BucketMetaData> metaArray = new ArrayList<BucketMetaData>();
-			metaArray.add(metaData);
-			b.setBucketMetaData(metaArray);
-		}
-
-		return b;
 	}
 
 	private static String[] generateAttAndLocQueries(String query, Set<String> locations, int attempt) {
@@ -269,61 +202,6 @@ public class AttributeBucketer {
 
 		SearchResponse sr = srb.get();
 		return sr;
-	}
-
-	private static List<Bucket> getBucketsFromSearchResponse(SearchResponse sr, String[] querySplit, int hitsInScroll,
-			int loops, Client client) {
-		int hitCounter = 0;
-		List<Bucket> bucketList = new ArrayList<Bucket>();
-		Set<String> hits = new HashSet<String>();
-		Set<String> misses = new HashSet<String>();
-		while ((hitCounter < hitsInScroll * loops) && (sr.getHits().getHits().length > 0)) {
-			LOGGER.debug(" query {}", sr.toString());
-
-			LOGGER.debug(" response {} {} {}", hitCounter, sr.getHits().getHits().length, sr.getTookInMillis());
-			for (SearchHit hit : sr.getHits()) {
-				try {
-					Bucket b = processHitsToBuckets(hit, querySplit[0], hits, misses);
-					if (querySplit.length > 1 && querySplit[1].length() > 1) {
-						BucketTerms bts = new BucketTerms(querySplit[1].toUpperCase() + LOCATION_CONTEXT, 100, true, 0);
-						b.getBucketTerms().add(bts);
-
-					} else if (b == null && querySplit[1].length() > 0) {
-						BucketTerms bts = new BucketTerms(querySplit[1].toUpperCase() + LOCATION_CONTEXT, 100, true, 0);
-						b = new Bucket(new HashSet(Arrays.asList(bts)), 1, 1, 0);
-
-					}
-					if (b != null) {
-						if (bucketList.contains(b)) {
-							bucketList.get(bucketList.indexOf(b)).incrementCount();
-							if (b.getBucketMetaData() != null) {
-								bucketList.get(bucketList.indexOf(b)).addMetaData(b.getBucketMetaData().get(0));
-							}
-
-						} else {
-							bucketList.add(b);
-						}
-						hitCounter++;
-					} else
-						hitCounter++;
-				} catch (Exception e) {
-					LOGGER.debug("Error processing row {}", e.getCause().getMessage());
-					LOGGER.debug("Hit Counter: " + hitCounter);
-					e.printStackTrace();
-				}
-			}
-			if (hitCounter < hitsInScroll) {
-				break;
-			}
-			if (hitCounter < hitsInScroll * loops) {
-				sr = client.prepareSearchScroll(sr.getScrollId()).setScroll(new TimeValue(160000)).get();
-			}
-		}
-
-		Collections.sort(bucketList);
-		LOGGER.debug(" list {}", bucketList);
-
-		return bucketList;
 	}
 
 }
